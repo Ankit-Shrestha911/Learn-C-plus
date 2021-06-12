270ebb4873f870111286a6a73e2a4194df6d82a2
Ankit Shrestha <74950690+Ankit-Shrestha911@users.noreply.github.com>
2021-06-12 15:55:45 +0530

    Update Array_Rotation1.cpp

--- Arrays/Array_Rotation1.cpp
+++ Arrays/Array_Rotation1.cpp
@@ -1,47 +1,61 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-void rotateArray(int *arr , int size, int rotate){
-    int temp[rotate-1];
-   for(int i=0; i<rotate; i++){
-       temp[i] = arr[i];
-   }
-
-   int j = rotate;
-   for(int i = 0; i<size-rotate; i++){
-       arr[i]  = arr[j];
-       j++;
-   }
-
-   int index = 0 ;
-  
-  for(int i=size-rotate; i<size; i++ ){
-      arr[i] = temp[index];
-      index++;
-  }
-
-  
-
-  
-
-
-}
- 
-int main() 
+void rotateArray(int a[], int size, int rot)
 {
-    int rotate;
-    int arr[] = {1,2,3,4,5,6,7};
-    int size = sizeof(arr)/sizeof(int);
-   cin>>rotate;
-
-    rotateArray(arr,size,rotate);
-
-    
-  for (int i = 0; i < size; i++)
-  {
-      cout<<arr[i]<<" ";
-  }
+  vector<int> help;
+  int i = 0;
+  int corret = rot % size;
   
-     
-    return 0;
+  int inner = size - corret;
+
+  if (rot % size == 0)
+  {
+    for (int j = 0; j < size; j++)
+    {
+      cout << a[j] << " ";
+    }
+
+    return;
+  }
+
+  for (i = 0; i < corret; i++)
+  {
+    help.push_back(a[i]);
+  }
+
+  for (int j = 0; j < size; j++)
+  {
+    a[j] = a[i++];
+  }
+
+  for (int k = 0; k < help.size(); k++)
+  {
+    a[inner++] = help[k];
+  }
+
+  for (int j = 0; j < size; j++)
+  {
+    cout << a[j] << " ";
+  }
+}
+
+int main()
+{
+  int arr[] = {1, 2, 3, 4, 5, 6};
+  rotateArray(arr, 6, 20);
+
+  /*
+    20%6 = 2
+    18 --> same array
+    we have to rotate only 2 times 
+    curr array after rotating 18 times it will remain same;
+    new 20-18 --> 2;
+    1,2,3,4,5,6
+    3,4,5,6,1,2 completing 20 rotation
+  */
+
+  return 0;
 }
